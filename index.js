93cf3976971f68d7e11394bb9b4dadc7fabe5e40
Ryan Burnette <ryan.burnette@gmail.com>
2019-06-07 16:48:15 -0400

    1.0.2
    - work with images

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var fs = require("fs");
+var fs = require('fs');
 var path = require('path');
 var util = require('util');
-var AWS = require("aws-sdk");
+var AWS = require('aws-sdk');
 var walk = require('walk').walk;
 var mime = require('mime-types');
 var readFile = util.promisify(fs.readFile);
 
-var s3 = new AWS.S3({ signatureVersion: 'v4' });
+var s3 = new AWS.S3({signatureVersion: 'v4'});
 
-var required = ['source','destination','bucket'];
+var required = ['source', 'destination', 'bucket'];
 
-module.exports = function (options) {
-  required.forEach(function (k) {
+module.exports = function(options) {
+  required.forEach(function(k) {
     if (!Object.keys(options).includes(k)) {
       throw new Error(`options.${k} is required`);
     }
@@ -30,44 +30,48 @@ module.exports = function (options) {
 
   // console.log(context,source,destination);process.exit();
 
-  walker.on('file', function (root, fileStats, next) {
+  walker.on('file', function(root, fileStats, next) {
     var filename = fileStats.name;
-    var fullpath = path.resolve(root,fileStats.name);
-    files.push(readFile(fullpath).then(function (buffer) {
-      return {
-        root,
-        relDir: root.replace(source,''),
-        filename,
-        fullpath,
-        buffer
-      };
-    }));
+    var fullpath = path.resolve(root, fileStats.name);
+    files.push(
+      readFile(fullpath).then(function(buffer) {
+        return {
+          root,
+          relDir: root.replace(source, ''),
+          filename,
+          fullpath,
+          buffer
+        };
+      })
+    );
     next();
   });
 
-  walker.on('end', () => {
-    Promise.all(files).then(function (results) {
-      results.forEach(function (f) {
+  walker.on('end', function () {
+    Promise.all(files).then(function(results) {
+      results.forEach(function(f) {
         // console.log(f);
-        var Key = path.join(releaseDir,f.relDir,f.filename);
-        var ContentType = mime.lookup(f.filename) || 'text/plain';
+        var Key = path.join(releaseDir, f.relDir, f.filename);
+        var ContentType = mime.lookup(f.filename) || 'application/octet-stream';
         // console.log(Key);
-        return s3.putObject({
-          Bucket,
-          Key,
-          Body: f.buffer.toString('binary'),
-          ACL,
-          ContentType,
-        }).promise()
-          .then(function (result) {
+        return s3
+          .putObject({
+            Bucket,
+            Key,
+            //Body: f.buffer.toString('binary'),
+            Body: f.buffer,
+            ACL,
+            ContentType
+          })
+          .promise()
+          .then(function(result) {
             console.log(`PUT ${Key}`);
             return result;
           })
-          .catch(function (err) {
+          .catch(function(err) {
             console.log(`ERR ${err.message} ${Key}`);
           });
       });
     });
   });
 };
-
